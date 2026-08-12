bd9ea7b2c9ceb5b91f51c814abdf2a3d231750d1
Hawra <hawraasharaf89@gmail.com>
2026-08-12 20:33:58 +0300

    added postForm :)

--- src/pages/PostForm.jsx
+++ src/pages/PostForm.jsx
@@ -5,10 +5,32 @@ import { useNavigate, useParams } from "react-router"
 const PostForm =()=>{
     const {challengeId} = useParams()
     const navigate = useNavigate()
+    const initialState = {
+        text: "",
+        img: "",
+    }
+    const [formData, setFormData]= useState(initialState)
+
+    const handleChange = (event) =>{
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault()
+        await postService.create(challengeId, formData)
+        navigate(`/challenges/${challengeId}/posts`)
+    }
 
     return(
         <div>
-            <h1>Post</h1>
+            <h1>Add a Post</h1>
+            <form onSubmit={handleSubmit}>
+                Text 
+                <input type="text" name="text" value={formData.text} onChange={handleChange} />
+                img
+                <input type="text" name="img" value={formData.img} onChange={handleChange} />
+                <button type="submit">Add Post</button>
+            </form>
         </div>
     )
 }
